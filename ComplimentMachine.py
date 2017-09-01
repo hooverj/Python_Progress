905d4c8a9d651e97f18a54a7b5bd48ee2561cdf7
Jordan Hoover <jordan@hoover.work>
2017-09-01 12:09:52 -0700

    Revert "Progress of learning python"
    This reverts commit 4a53fca7d565ef81be61a8c1ced5f80c8e329a76.

--- ComplimentMachine.py
+++
@@ -1,8 +0,0 @@
-from random import *
-
-print("Welcome to Jordan's AMAZING compliment machine.")
-print("---------------------------")
-name = input('What is your name?:')
-compliments = [ "you are excellent and above average." , "good going on being so great all the time." , "your outfit really brings out your eyes." , "you are looking lovely today." , "are you pregnant? Cuz you're glowing ;).", "Do you have car insurance? Cuz I wanna hit it from the back." ]
-print("Dear", name)
-print(choice(compliments))
